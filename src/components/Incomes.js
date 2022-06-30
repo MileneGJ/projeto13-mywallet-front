87a76c63338ea05707d235eca866becae736947b
milenegj <milene.jannetti@gmail.com>
2022-06-29 20:26:08 -0500

    Layout Transactions, Incomes and Outcomes pages

--- src/components/Incomes.js
+++ src/components/Incomes.js
@@ -1,6 +1,51 @@
-function Incomes () {
+import styled from 'styled-components'
 
+function Incomes () {
+return (        
+<Container>
+    <h1>Nova entrada</h1>
+    <form>
+        <input type="currency" placeholder="Valor" />
+        <input type="text" placeholder="Descrição" />
+        <button type="submit">Salvar entrada</button>
+    </form>
+</Container>
+)
 }
 
+const Container = styled.div`
+padding:25px;
+box-sizing:border-box;
+h1{
+    color:#FFFFFF;
+    font-size:26px;
+    font-weight:bold;
+    margin-bottom:20px;
+    height:44px;
+}
+input{
+    height:60px;
+    font-size:20px;
+    font-family: 'Raleway', sans-serif;
+    color:#000000;
+    margin:8px 0;
+    border-radius:5px;
+    padding:10px;
+    box-sizing:border-box;
+    border:none;
+}
+button{
+    height:46px;
+    background-color:#A328D6;
+    color:#FFFFFF;
+    font-weight:bold;
+    text-align:center;
+    font-size:20px;
+    border:none;
+    border-radius:5px;
+    margin:8px 0;
+    box-sizing:border-box;
+}
+`
 
 export default Incomes
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,6 +1,68 @@
-function Transactions () {
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
+function Transactions() {
+    return(
+    <Container>
+        <Header>
+            <h1>Olá, Fulano</h1>
+            <ion-icon name="log-out-outline"></ion-icon>
+        </Header>
+        <History>Não há registros de <br/> entrada ou saída</History>
+        <AddTransaction>
+            <Link to="/income">
+                <ion-icon name="add-circle-outline"></ion-icon>
+                <p>Nova entrada</p>
+            </Link>
+            <Link to="/outcome">
+                <ion-icon name="remove-circle-outline"></ion-icon>
+                <p>Nova saída</p>
+            </Link>
+        </AddTransaction>
+    </Container>
+    )
 }
-
+const Container = styled.div`
+padding:25px;
+box-sizing:border-box;
+div{
+    margin:7px 0;
+    box-sizing:border-box;
+}
+`
+const Header = styled.div`
+height:44px;
+display:flex;
+justify-content:space-between;
+align-items:center;
+ion-icon{
+    font-size:34px;
+    color:#FFFFFF;
+}
+`
+const History = styled.div`
+background-color:#FFFFFF;
+border-radius:5px;
+height:80vh;
+font-size:20px;
+color:#868686;
+`
+const AddTransaction = styled.div`
+display:flex;
+justify-content:space-between
+div{
+    height:15vh;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    align-items:flex-start;
+    color:#FFFFFF;
+    background-color:#A328D6;
+}
+ion-icon{
+    font-size:16px;
+    color:#FFFFFF;
+}
+`
 
 export default Transactions
